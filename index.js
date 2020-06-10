f3f94157629a422136c244e675a530c06f69ae24
Dishant Kumar <dishant@dealshare.in>
2020-06-11 02:21:27 +0530

    data from json

--- index.js
+++ index.js
@@ -1,58 +1,23 @@
 
 const getYear = () => {
   const year = document.getElementById("year").value;
-  if (!!year.length && !isNaN(year)) {
+  if (!!year.length && !isNaN(year) && !(year < 1000 || year > 9999)) {
     return year
   } else {
-    alert('Please Enter valid Year');
+    alert('Please Enter valid Year \nProvide Four digit year (between 1000 and 9999)');
     return false
   }
 }
 
 const getDaysList = () => {
-  const bdayData = [
-    {
-      "name": "Cierra Vega",
-      "birthday": "7/20/1990"
-    }, {
-      "name": "Alden Cantrell",
-      "birthday": "2/23/1998"
-    }, {
-      "name": "Kierra Gentry",
-      "birthday": "4/4/1967"
-    }, {
-      "name": "Pierre Cox",
-      "birthday": "11/12/1996"
-    }, {
-      "name": "Thomas Crane",
-      "birthday": "10/5/1997"
-    }, {
-      "name": "Miranda Shaffer",
-      "birthday": "3/12/1998"
-    }, {
-      "name": "Bradyn Kramer",
-      "birthday": "4/5/1992"
-    }, {
-      "name": "Alvaro Mcgee",
-      "birthday": "12/19/1987"
-    }, {
-      "name": "Pierre Cox",
-      "birthday": "3/19/1946"
-    }, {
-      "name": "Thomas Crane",
-      "birthday": "1/5/1977"
-    }, {
-      "name": "Miranda Shaffer",
-      "birthday": "9/12/1988"
-    }, {
-      "name": "Bradyn Kramer",
-      "birthday": "10/5/1982"
-    }, {
-      "name": "Alvaro Mcgee",
-      "birthday": "1/19/1997"
-    }
-  ];
-  var days = [[], [], [], [], [], [], []]
+  let bdayData = null
+  try {
+    bdayData = JSON.parse(document.getElementById("bdayJson").value)
+  } catch {
+    alert('Enter valid Json')
+    return;
+  }
+  var days = [[], [], [], [], [], [], []] // list of Bday for all 7 days
   var year = getYear();
   if (!year) { return }
   bdayData.map(bday => {
@@ -78,7 +43,7 @@ updateDays = () => {
   var bdayBoxes = document.querySelectorAll('.bdayBox');
   for (var i = 0; i < 7; i++) {
     bdayBoxes[i].innerHTML = "";
-    boxSize = 150 / (Math.ceil(Math.sqrt(days[i].length)) || 1);
+    boxSize = 150 / (Math.ceil(Math.sqrt(days[i].length)) || 1); // get the sizes for box [by getting next perfect square number]
     if (days[i].length) {
 
       for (var j = 0; j < days[i].length; j++) {
@@ -87,6 +52,8 @@ updateDays = () => {
         bdayDiv.className = "bday";
         bdayDiv.style.width = boxSize;
         bdayDiv.style.height = boxSize;
+        var color = "#" + Math.floor(100000 + (Math.random() * 900000));
+        bdayDiv.style.backgroundColor = color;
         bdayBoxes[i].appendChild(bdayDiv);
       }
     } else {
@@ -96,24 +63,70 @@ updateDays = () => {
       noBdayImg.setAttribute("src", "./assets/sad_face.png")
       noBdayImg.className = "noBdaySadFace";
       noBdayDiv.appendChild(noBdayImg);
-      // element.className = "nobday";
       bdayBoxes[i].appendChild(noBdayDiv);
     }
   }
 
 }
 
-const updateColors = () => {
-  var bdayDivs = document.querySelectorAll('.bday');
-  if (bdayDivs.length) {
-    for (var i = 0; i < bdayDivs.length; i++) {
-      var color = "#" + Math.floor(100000 + (Math.random() * 900000));
-      bdayDivs[i].style.backgroundColor = color;
-    }
-  }
-}
-
 const update = () => {
   updateDays()
-  updateColors()
+}
+
+const addDefaultJson = () => {
+  document.getElementById("bdayJson").value = `
+    [
+      {
+        "name": "Cierra Vega",
+        "birthday": "7/20/1990"
+      },
+      {
+        "name": "Alden Cantrell",
+        "birthday": "2/23/1998"
+      },
+      {
+        "name": "Kierra Gentry",
+        "birthday": "4/4/1967"
+      },
+      {
+        "name": "Pierre Cox",
+        "birthday": "11/12/1996"
+      },
+      {
+        "name": "Thomas Crane",
+        "birthday": "10/5/1997"
+      },
+      {
+        "name": "Miranda Shaffer",
+        "birthday": "3/12/1998"
+      },
+      {
+        "name": "Bradyn Kramer",
+        "birthday": "4/5/1992"
+      },
+      {
+        "name": "Alvaro Mcgee",
+        "birthday": "12/19/1987"
+      },
+      {
+        "name": "Pierre Cox",
+        "birthday": "3/19/1946"
+      },
+      {
+        "name": "Thomas Crane",
+        "birthday": "1/5/1977"
+      },
+      {
+        "name": "Miranda Shaffer",
+        "birthday": "9/12/1988"
+      },
+      {
+        "name": "Bradyn Kramer",
+        "birthday": "10/5/1982"
+      },
+      {
+        "name": "Alvaro Mcgee",
+        "birthday": "1/19/1997"
+      }
+    ]`
 }
